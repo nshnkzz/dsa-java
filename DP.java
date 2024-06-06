c32a5fa92e99ad7fc97f1af140d60becdd521c90
Shashank Patwa <nshnkzz@gmail.com>
2024-06-06 10:02:29 +0530

    ncr binomial coefficient

--- DP.java
+++ DP.java
@@ -491,4 +491,29 @@ public class DP {
         HashMap<String, Integer> dp = new HashMap<>();
         return utility(S, 0, N - 1, true, dp);
     }
+
+    /**
+     * https://www.geeksforgeeks.org/problems/ncr1019/1
+     * @param n
+     * @param r
+     * @return
+     */
+    public static int nCr(int n, int r)
+    {
+        // binomial coefficient using pascal's triangle
+        int dp[] = new int[r+1];
+        dp[0] = 1;
+        if(r>n) return 0;
+        if(r==0||r==n) return 1;
+        if(r>n-r) r = n-r;
+        int mod = 1000000000+7;
+        for(int i = 0; i<=n; i++)
+        {
+            for(int j=Math.min(i,r); j>0; j--)
+            {
+                dp[j] = (dp[j] + dp[j-1]) % mod;
+            }
+        }
+        return dp[r];
+    }
 }
